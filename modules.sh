32e64270ea02af6c5ee0a37d6553afe150b8ae6e
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-11-04 15:54:17 +0100

    openmpi 4.1.1 and PartitionedArrays 0.2.6 this combo seems to work

--- modules.sh
+++ modules.sh
@@ -1,4 +1,4 @@
-module load openmpi/4.0.3
+module load openmpi/4.1.1
 export PATH=/home/552/fv3851/Apps/julia-1.6.3/bin:$PATH
 export JULIA_MPI_BINARY=system
-export JULIA_MPI_PATH=/apps/openmpi/4.0.3
+export JULIA_MPI_PATH=/apps/openmpi/4.1.1
